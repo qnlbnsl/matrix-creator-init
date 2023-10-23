397c1715f541c6e6c124f7ae7707099abbfcb4e4
Kunal Bansal <46204848+qnlbnsl@users.noreply.github.com>
2023-10-23 13:23:52 -0400

    Update to use python3

--- matrixlabs_edit_settings.py
+++ matrixlabs_edit_settings.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 
-' Modify Raspberry Pi settins '
+# Modify Raspberry Pi settings
 
 import sys
-
 
 # BUGS: We don't uncomment lines and instead add a new line with the
 # required value.
@@ -32,25 +31,25 @@ ORIGINAL_LINES = read_lines(sys.argv[1])
 
 KEYS_TO_CHANGE = {}
 for new_line in read_lines(sys.argv[2]):
-    # We throw away comments in the modifications. It's not wasy to place them!
+    # We throw away comments in the modifications. It's not easy to place them!
     if new_line[0] == 'SETTING':
         key, value = key_and_value(new_line)
         KEYS_TO_CHANGE[key] = value
 
 for original in ORIGINAL_LINES:
     if original[0] == 'REMARK':
-        print original[1]
+        print(original[1])
     else:
         key, value = key_and_value(original)
         if key in KEYS_TO_CHANGE and KEYS_TO_CHANGE[key] == value:
-            print '{}={}'.format(key, KEYS_TO_CHANGE[key])
+            print('{}={}'.format(key, KEYS_TO_CHANGE[key]))
             del KEYS_TO_CHANGE[key]
         else:
-            print original[1]
+            print(original[1])
 
 if len(KEYS_TO_CHANGE):
-    print '# Lines added by matrixlabs_edit_settings.py.'
-    print '# Commented definitions of the settings might be above.'
+    print('# Lines added by matrixlabs_edit_settings.py.')
+    print('# Commented definitions of the settings might be above.')
 
-    for key  in sorted(KEYS_TO_CHANGE):
-        print '{}={}'.format(key, KEYS_TO_CHANGE[key])
+    for key in sorted(KEYS_TO_CHANGE):
+        print('{}={}'.format(key, KEYS_TO_CHANGE[key]))
